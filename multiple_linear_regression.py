31fd988bef1fc82ed0c16bfa8ea7a4c142922a5c
Apoorv Patne <31184004+apoorvpatne10@users.noreply.github.com>
2018-01-14 18:34:38 +0530

    Add files via upload

---
+++ multiple_linear_regression.py
@@ -0,0 +1,52 @@
+# Multiple Linear Regression
+
+# Analysis of 50 Companies (dataset) and create a model that'll tell the venture capitalist fund which 
+# type of companies it is most interested and their main criteria is profit 
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('50_Startups.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 4].values
+
+# Encoding categorical data
+# Encoding the Independent Variable
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_X = LabelEncoder()
+X[:, 3] = labelencoder_X.fit_transform(X[:, 3])
+onehotencoder = OneHotEncoder(categorical_features = [3])
+X = onehotencoder.fit_transform(X).toarray()
+
+#Avoiding the dummy variable trap
+X = X[:, 1:]
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+# Feature Scaling
+"""from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+sc_y = StandardScaler()
+y_train = sc_y.fit_transform(y_train)"""
+
+#Fitting multiple linear regression to the training set
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+#Predicting the test series
+y_pred = regressor.predict(X_test)
+
+#OLS : Ordinary Square Algorithm
+#Building the optimal model using backward elimination method
+import statsmodels.formula.api as sm
+X = np.append(arr = np.ones((50, 1)).astype(int), values = X, axis = 1)
+X_opt = X[:, [0, 1, 2, 3, 4, 5]]  
+regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
